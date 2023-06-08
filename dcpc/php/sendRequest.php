1d16b433e2c14b23262d80720f75560ac02fe3b0
jritter77 <jr550@humboldt.edu>
2023-06-08 14:07:37 -0700

    - email

--- dcpc/php/sendRequest.php
+++ dcpc/php/sendRequest.php
@@ -5,9 +5,9 @@ $req = json_decode($_POST['req']);
 $to = 'jakeritter77@gmail.com';
 $sub = $req->sub;
 $msg = $req->msg;
-$headers = 'From: jr550@humboldt.edu' . "\r\n";
+$headers = 'From: Jake Ritter <jr550@humboldt.edu>' . "\r\n";
 
-$result = mail($to, $sub, $msg, $headers, "-fjr550@humbodlt.edu");
+$result = mail($to, $sub, $msg, $headers);
 
 echo json_encode($result);
 
