fd648363e29bd2f646527bb42a1c09acb1b460c0
jritter77 <jr550@humboldt.edu>
2023-06-08 14:11:44 -0700

    - mail

--- dcpc/php/sendRequest.php
+++ dcpc/php/sendRequest.php
@@ -9,8 +9,6 @@ $msg = $req->msg;
 $fromName = "Jake Ritter";
 $fromEmail = "jriveserver@gmail.com"
 
-$headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .= 'From:  ' . $fromName . ' <' . $fromEmail .'>' . " \r\n" .
             'Reply-To: '.  $fromEmail . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
